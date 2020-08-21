4b8d3f69104e00fa91eced46b9a5406b13a2f36e
Pietra Ferreira <pietra.ferreira@embecosm.com>
2020-08-21 08:45:47 +0100

    Added support for CORE-V hardware loop
    Added two relocations REL12 and RELU5, the CORE-V INSN_CLASSes, the MATCH
    and MASKs and the hardware loop instructions.
    
    bfd/ChangeLog:
    
    	* bfd-in2.h: Added CORE-V hardware loop specific relocations.
    	* elfnn-riscv.c: Added relocations for CORE-V hardware loop.
    	* elfxx-riscv.c: Added relocations and amend howto lookup.
    	* riscv.c: Added BFD_RELOC_RISCV_CVPCREL_UI12 and
    	BFD_RELOC_RISCV_CVPCREL_URS1.
    
    gas/ChangeLog:
    
    	* config/tc-riscv.c: Added CORE-V harware loop support.
    	* config/tc-riscv.h: Likewise.
    	* doc/c-riscv.texi: Noted Xcorev as additional ISA extension
    	for CORE-V.
    
    gas/testsuite/ChangeLog:
    
    	* gas/riscv/cv-hwloop-01.d: Added new test.
    	* gas/riscv/cv-hwloop-01.l: Likewise.
    	* gas/riscv/cv-hwloop-01.s: Likewise.
    	* gas/riscv/cv-hwloop-02.d: Likewise.
    	* gas/riscv/cv-hwloop-02.l: Likewise.
    	* gas/riscv/cv-hwloop-02.s: Likewise.
    	* gas/riscv/cv-hwloop-03.d: Likewise.
    	* gas/riscv/cv-hwloop-03.l: Likewise.
    	* gas/riscv/cv-hwloop-03.s: Likewise.
    	* gas/riscv/cv-hwloop-04.d: Likewise.
    	* gas/riscv/cv-hwloop-04.l: Likewise.
    	* gas/riscv/cv-hwloop-04.s: Likewise.
    	* gas/riscv/cv-hwloop-05.d: Likewise.
    	* gas/riscv/cv-hwloop-05.l: Likewise.
    	* gas/riscv/cv-hwloop-05.s: Likewise.
    	* gas/riscv/cv-hwloop-06.d: Likewise.
    	* gas/riscv/cv-hwloop-06.l: Likewise.
    	* gas/riscv/cv-hwloop-06.s: Likewise.
    	* gas/riscv/cv-hwloop-07.d: Likewise.
    	* gas/riscv/cv-hwloop-07.l: Likewise.
    	* gas/riscv/cv-hwloop-07.s: Likewise.
    	* gas/riscv/cv-hwloop-08.d: Likewise.
    	* gas/riscv/cv-hwloop-08.l: Likewise.
    	* gas/riscv/cv-hwloop-08.s: Likewise.
    	* gas/riscv/cv-hwloop-count.d: Likewise.
    	* gas/riscv/cv-hwloop-count.s: Likewise.
    	* gas/riscv/cv-hwloop-counti.d: Likewise.
    	* gas/riscv/cv-hwloop-counti.s: Likewise.
    	* gas/riscv/cv-hwloop-endi.d: Likewise.
    	* gas/riscv/cv-hwloop-endi.s: Likewise.
    	* gas/riscv/cv-hwloop-setup.d: Likewise.
    	* gas/riscv/cv-hwloop-setup.s: Likewise.
    	* gas/riscv/cv-hwloop-setupi.d: Likewise.
    	* gas/riscv/cv-hwloop-setupi.s: Likewise.
    	* gas/riscv/cv-hwloop-starti.d: Likewise.
    	* gas/riscv/cv-hwloop-starti.s: Likewise.
    	* gas/riscv/cv-hwloop-09.d: Likewise.
    	* gas/riscv/cv-hwloop-09.l: Likewise.
    	* gas/riscv/cv-hwloop-09.s: Likewise.
    	* gas/riscv/cv-hwloop-10.d: Likewise.
    	* gas/riscv/cv-hwloop-10.l: Likewise.
    	* gas/riscv/cv-hwloop-10.s: Likewise.
    
    include/ChangeLog:
    
    	* elf/riscv.h: Added CORE-V hardware loop specific relocations.
    	* opcode/riscv.h: Added CORE-V hardware loop specific masks and
    	CORE-V instruction class. Added macros for unsigned I type
    	immediate and loop number.
    
    ld/ChangeLog:
    
    	* emultempl/riscvelf.em: Added initial CORE-V support.
    
    ld/testsuite/ChangeLog:
    
    	* ld-riscv-elf/ld-riscv-elf.exp: Added CORE-V hardware loop
    	  tests.
    	* ld-riscv-elf/cv-hwloop-starti.s: Added new test.
    	* ld-riscv-elf/cv-hwloop-starti.d: Likewise.
    	* ld-riscv-elf/cv-hwloop-endi.d: Likewise.
    	* ld-riscv-elf/cv-hwloop-endi.s: Likewise.
    	* ld-riscv-elf/cv-hwloop-setup.d: Likewise.
    	* ld-riscv-elf/cv-hwloop-setup.s: Likewise.
    	* ld-riscv-elf/cv-hwloop-setupi.d: Likewise.
    	* ld-riscv-elf/cv-hwloop-setupi.s: Likewise.
    
    opcodes/ChangeLog:
    
    	* riscv-dis.c: Added CORE-V hardware loop support.
    	* riscv-opc.c: Likewise.
    
    Signed-off-by: Mary Bennett <mary.bennett@embecosm.com>

--- include/opcode/riscv.h
+++ include/opcode/riscv.h
@@ -2,6 +2,13 @@
    Copyright (C) 2011-2021 Free Software Foundation, Inc.
    Contributed by Andrew Waterman
 
+   Modified for CORE-V by:
+   Mary Bennett (mary.bennett@embecosm.com)
+   Pietra Ferreira (pietra.ferreira@embecosm.com)
+   Jessica Mills (jessica.mills@embecosm.com)
+
+   Some of these changes are (C) Open Hardware Group, pending FSF assignment.
+
    This file is part of GDB, GAS, and the GNU binutils.
 
    GDB, GAS, and the GNU binutils are free software; you can redistribute
@@ -102,6 +109,14 @@ static const char * const riscv_pred_succ[16] =
 #define EXTRACT_CJTYPE_IMM(x) \
   ((RV_X(x, 3, 3) << 1) | (RV_X(x, 11, 1) << 4) | (RV_X(x, 2, 1) << 5) | (RV_X(x, 7, 1) << 6) | (RV_X(x, 6, 1) << 7) | (RV_X(x, 9, 2) << 8) | (RV_X(x, 8, 1) << 10) | (-RV_X(x, 12, 1) << 11))
 
+/* CORE-V Specific.  */
+#define EXTRACT_I1TYPE_UIMM(x) \
+  (RV_X(x, 15, 5))
+#define EXTRACT_I1TYPE_LN(x) \
+  (RV_X(x, 7, 1))
+#define EXTRACT_ITYPE_UIMM(x) \
+  (RV_X(x, 20, 12))
+
 #define ENCODE_ITYPE_IMM(x) \
   (RV_X(x, 0, 12) << 20)
 #define ENCODE_STYPE_IMM(x) \
@@ -142,6 +157,12 @@ static const char * const riscv_pred_succ[16] =
   ((RV_X(x, 1, 2) << 3) | (RV_X(x, 3, 2) << 10) | (RV_X(x, 5, 1) << 2) | (RV_X(x, 6, 2) << 5) | (RV_X(x, 8, 1) << 12))
 #define ENCODE_CJTYPE_IMM(x) \
   ((RV_X(x, 1, 3) << 3) | (RV_X(x, 4, 1) << 11) | (RV_X(x, 5, 1) << 2) | (RV_X(x, 6, 1) << 7) | (RV_X(x, 7, 1) << 6) | (RV_X(x, 8, 2) << 9) | (RV_X(x, 10, 1) << 8) | (RV_X(x, 11, 1) << 12))
+
+/* CORE-V Specific.  */
+#define ENCODE_I1TYPE_UIMM(x) \
+  (RV_X(x, 0, 5) << 15)
+#define ENCODE_I1TYPE_LN(x) \
+  (RV_X(x, 0, 1) << 7)
 
 #define VALID_ITYPE_IMM(x) (EXTRACT_ITYPE_IMM(ENCODE_ITYPE_IMM(x)) == (x))
 #define VALID_STYPE_IMM(x) (EXTRACT_STYPE_IMM(ENCODE_STYPE_IMM(x)) == (x))
@@ -229,6 +250,14 @@ static const char * const riscv_pred_succ[16] =
 #define OP_SH_AQ		26
 #define OP_MASK_RL		0x1
 #define OP_SH_RL		25
+
+/* CORE-V Specific.  */
+#define OP_MASK_IMM12           0xfff
+#define OP_SH_IMM12             20
+#define OP_MASK_IMM5            0x1f
+#define OP_SH_IMM5              15
+#define OP_MASK_LN              0x1
+#define OP_SH_LN                7
 
 #define OP_MASK_CSR		0xfffU
 #define OP_SH_CSR		20
@@ -319,6 +348,7 @@ enum riscv_insn_class
   INSN_CLASS_ZBA,
   INSN_CLASS_ZBB,
   INSN_CLASS_ZBC,
+  INSN_CLASS_COREV,
 };
 
 /* This structure holds information for a particular instruction.  */
