7fb201b34396cec4cd38d36b7d6ed9e9326f7538
Jessica Mills <jessica.mills@embecosm.com>
2020-11-19 16:51:09 +0000

    Added new boundary tests & fixed boundary check bug in hwloop & mac
    gas/ChangeLog.COREV:
    
    	* config/tc-riscv.c: Fixed bug in hardware loop operand
    	boundary checks (b1 and b2).
    
    gas/testsuite/ChangeLog.COREV:
    
    	* cv-hwloop-16.d: Added new boundary test.
    	* cv-hwloop-16.l: Likewise.
    	* cv-hwloop-16.s: Likewise.
    	* cv-hwloop-17.d: Likewise.
    	* cv-hwloop-17.l: Likewise.
    	* cv-hwloop-17.s: Likewise.
    	* cv-hwloop-18.d: Likewise.
    	* cv-hwloop-18.l: Likewise.
    	* cv-hwloop-18.s: Likewise.
    	* cv-hwloop-19.d: Likewise.
    	* cv-hwloop-19.l: Likewise.
    	* cv-hwloop-19.s: Likewise.
    	* cv-hwloop-20.d: Likewise.
    	* cv-hwloop-20.l: Likewise.
    	* cv-hwloop-20.s: Likewise.
    	* cv-hwloop-21.d: Likewise.
    	* cv-hwloop-21.l: Likewise.
    	* cv-hwloop-21.s: Likewise.
    	* cv-hwloop-22.d: Likewise.
    	* cv-hwloop-22.l: Likewise.
    	* cv-hwloop-22.s: Likewise.
    	* cv-hwloop-23.d: Likewise.
    	* cv-hwloop-23.l: Likewise.
    	* cv-hwloop-23.s: Likewise.
    	* cv-hwloop-09.s: Fixed incorrect comment.
    	* cv-hwloop-10.s: Likewise.
    	* cv-mac-07.d: Added new boundary test.
    	* cv-mac-07.l: Likewise.
    	* cv-mac-07.s: Likewise.
    	* cv-mac-08.d: Likewise.
    	* cv-mac-08.l: Likewise.
    	* cv-mac-08.s: Likewise.
    	* cv-mac-05.l: Changed to non-boundary value.
    	* cv-mac-05.s: Likewise.
    
    Signed-off-by: Jessica Mills <jessica.mills@embecosm.com>

---
+++ gas/testsuite/gas/riscv/cv-hwloop-16.d
@@ -0,0 +1,3 @@
+#as: -march=rv32i_xcorevhwlp
+#source: cv-hwloop-16.s
+#error_output: cv-hwloop-16.l
---
+++ gas/testsuite/gas/riscv/cv-hwloop-21.d
@@ -0,0 +1,3 @@
+#as: -march=rv32i_xcorevhwlp
+#source: cv-hwloop-21.s
+#error_output: cv-hwloop-21.l
