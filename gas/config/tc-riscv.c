349e8fc58c70a9a71a50b6a288737c151e66f12e
Jessica Mills <jessica.mills@embecosm.com>
2020-11-19 16:51:09 +0000

    Added new boundary tests & fixed boundary check bug in hwloop & mac
    gas/ChangeLog.COREV:
    
    	* config/tc-riscv.c: Fixed bug in hardware loop operand
    	boundary checks (b1 and b2).
    
    gas/testsuite/ChangeLog.COREV:
    
    	* cv-hwloop-16.d: Added new boundary test.
    	* cv-hwloop-16.l: Likewise.
    	* cv-hwloop-16.s: Likewise.
    	* cv-hwloop-17.d: Likewise.
    	* cv-hwloop-17.l: Likewise.
    	* cv-hwloop-17.s: Likewise.
    	* cv-hwloop-18.d: Likewise.
    	* cv-hwloop-18.l: Likewise.
    	* cv-hwloop-18.s: Likewise.
    	* cv-hwloop-19.d: Likewise.
    	* cv-hwloop-19.l: Likewise.
    	* cv-hwloop-19.s: Likewise.
    	* cv-hwloop-20.d: Likewise.
    	* cv-hwloop-20.l: Likewise.
    	* cv-hwloop-20.s: Likewise.
    	* cv-hwloop-21.d: Likewise.
    	* cv-hwloop-21.l: Likewise.
    	* cv-hwloop-21.s: Likewise.
    	* cv-hwloop-22.d: Likewise.
    	* cv-hwloop-22.l: Likewise.
    	* cv-hwloop-22.s: Likewise.
    	* cv-hwloop-23.d: Likewise.
    	* cv-hwloop-23.l: Likewise.
    	* cv-hwloop-23.s: Likewise.
    	* cv-hwloop-09.s: Fixed incorrect comment.
    	* cv-hwloop-10.s: Likewise.
    	* cv-mac-07.d: Added new boundary test.
    	* cv-mac-07.l: Likewise.
    	* cv-mac-07.s: Likewise.
    	* cv-mac-08.d: Likewise.
    	* cv-mac-08.l: Likewise.
    	* cv-mac-08.s: Likewise.
    	* cv-mac-05.l: Changed to non-boundary value.
    	* cv-mac-05.s: Likewise.
    
    Signed-off-by: Jessica Mills <jessica.mills@embecosm.com>

--- gas/config/tc-riscv.c
+++ gas/config/tc-riscv.c
@@ -3005,7 +3005,7 @@ riscv_ip (char *str, struct riscv_cl_insn *ip, expressionS *imm_expr,
 		  if (imm_expr->X_op == O_constant)
 		    {
 		      if (imm_expr->X_add_number < 0 ||
-			  ((imm_expr->X_add_number>>1) > 0x0FFF))
+			  ((imm_expr->X_add_number>>1) > 0x07FF))
 			as_bad (_("%ld constant out of range for %s, range:[0, %d]"),
 				imm_expr->X_add_number, ip->insn_mo->name, 0xFFE);
 		      if ((imm_expr->X_add_number % 2) == 1)
@@ -3032,7 +3032,7 @@ riscv_ip (char *str, struct riscv_cl_insn *ip, expressionS *imm_expr,
 		  if (imm_expr->X_op == O_constant)
 		    {
 		      if (imm_expr->X_add_number < 0 ||
-			  ((imm_expr->X_add_number>>1) > 31))
+			  ((imm_expr->X_add_number>>1) > 0xF))
 			as_bad (_("%ld constant out of range for "
 				  "cv.setupi, range:[0, %d]"),
 				imm_expr->X_add_number, 0x1E);
--- gas/testsuite/gas/riscv/cv-hwloop-10.s
+++ gas/testsuite/gas/riscv/cv-hwloop-10.s
@@ -1,3 +1,3 @@
-# Loop count must be positive integer in range:[0, 4094]
+# Loop count must be positive integer in range:[0, 4095]
 target:
 	cv.setupi 0, 9000, 4
