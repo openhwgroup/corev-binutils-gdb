863add7c84ffa8ed61860424f3417b39ce64e72a
Nazareno Bruschi <nazareno.bruschi@studio.unibo.it>
2020-11-20 18:43:10 +0100

    Added post-incrementing and reg-reg loads

--- include/opcode/riscv.h
+++ include/opcode/riscv.h
@@ -343,7 +343,8 @@ enum riscv_insn_class
    INSN_CLASS_F_AND_C,
    INSN_CLASS_Q,
    INSN_CLASS_COREV_HWLP,
-   INSN_CLASS_COREV_MAC
+   INSN_CLASS_COREV_MAC,
+   INSN_CLASS_COREV_POSTINC
   };
 
 /* This structure holds information for a particular instruction.  */
