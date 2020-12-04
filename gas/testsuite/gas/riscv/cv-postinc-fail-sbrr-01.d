cae386e6e21d08774ead2afa59fcaf5068f83571
enrico <enrico.tabanelli3@unibo.it>
2020-12-04 12:13:06 +0100

    Adding Failing Tests for rr,rrpost,immpost with b,h,w

---
+++ gas/testsuite/gas/riscv/cv-postinc-fail-sbrr-01.d
@@ -0,0 +1,3 @@
+#as: -march=rv32i_xcorevpostinc
+#source: cv-postinc-fail-sbrr-01.s
+#error_output: cv-postinc-fail-sbrr-01.l
---
+++ gas/testsuite/gas/riscv/cv-postinc-fail-shimmpost-02.d
@@ -0,0 +1,3 @@
+#as: -march=rv32i_xcorevpostinc
+#source: cv-postinc-fail-shimmpost-02.s
+#error_output: cv-postinc-fail-shimmpost-02.l
