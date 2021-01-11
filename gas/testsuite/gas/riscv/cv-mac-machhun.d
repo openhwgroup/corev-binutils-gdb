453419cd8fc0e8d2a5f03b90eb8261c142444999
Jessica Mills <jessica.mills@embecosm.com>
2021-01-11 21:43:25 +0000

    Removed obsolete MAC instructions
    gas/testsuite/ChangeLog.COREV:
    
    	* gas/riscv/cv-mac-machhsn.d: Removed obsolete instruction cv.machhs.
    	* gas/riscv/cv-mac-machhun.d: Removed obsolete instruction cv.machhu.
    	* gas/riscv/cv-mac-macsn.d: Removed obsolete instruction cv.macs.
    	* gas/riscv/cv-mac-macun.d: Removed obsolete instruction cv.macu.
    
    include/ChangeLog.COREV:
    
    	* opcode/riscv-opc.h: Removed obsolete matches and renamed mask
    	accordingly -
    	MATCH_MACS:	Removed.
    	MATCH_MACHHS:	Removed.
    	MATCH_MACU:	Removed.
    	MATCH_MACHHU:	Removed.
    	MASK_MULMAC ->	MASK_MULSH
    
    opcodes/ChangeLog.COREV:
    
    	* riscv-opc.c: Removed obsolete instructions -
    	cv.macs:	Removed.
    	cv.machhs:	Removed.
    	cv.macu:	Removed.
    	cv.machhu:	Removed.
    
    Signed-off-by: Jessica Mills <jessica.mills@embecosm.com>

--- gas/testsuite/gas/riscv/cv-mac-machhun.d
+++ gas/testsuite/gas/riscv/cv-mac-machhun.d
@@ -7,6 +7,6 @@
 Disassembly of section .text:
 
 0+000 <target>:
-[ 	]+0:[ 	]+407312db[ 	]+cv.machhu[ 	]+t0,t1,t2
+[ 	]+0:[ 	]+407312db[ 	]+cv.machhun[ 	]+t0,t1,t2,0
 [ 	]+4:[ 	]+64539edb[ 	]+cv.machhun[ 	]+t4,t2,t0,18
 [ 	]+8:[ 	]+7e6f1e5b[ 	]+cv.machhun[ 	]+t3,t5,t1,31
--- gas/testsuite/gas/riscv/cv-mac-macun.d
+++ gas/testsuite/gas/riscv/cv-mac-macun.d
@@ -7,6 +7,6 @@
 Disassembly of section .text:
 
 0+000 <target>:
-[ 	]+0:[ 	]+007312db[ 	]+cv.macu[ 	]+t0,t1,t2
+[ 	]+0:[ 	]+007312db[ 	]+cv.macun[ 	]+t0,t1,t2,0
 [ 	]+4:[ 	]+36539edb[ 	]+cv.macun[ 	]+t4,t2,t0,27
 [ 	]+8:[ 	]+3e6f1e5b[ 	]+cv.macun[ 	]+t3,t5,t1,31
