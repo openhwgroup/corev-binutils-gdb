1772262abda72bf32d81000a75a174a63148c1e9
Jessica Mills <jessica.mills@embecosm.com>
2021-02-19 13:10:19 +0000

    Fix whitespace inconsistencies
    gas/testsuite/ChangeLog.COREV:
    
    	* gas/riscv/cv-hwlp-march-rv32i-xcorev.s: Fix whitespace
    	inconsistencies.
    
    opcodes/ChangeLog.COREV:
    
    	* riscv-dis.c: Fix whitespace inconsistency.
    
    Signed-off-by: Jessica Mills <jessica.mills@embecosm.com>

--- opcodes/riscv-dis.c
+++ opcodes/riscv-dis.c
@@ -279,7 +279,7 @@ print_insn_args (const char *d, insn_t l, bfd_vma pc, disassemble_info *info)
 	      info->target = (EXTRACT_ITYPE_IMM (l)<<1) + pc; ++d;
 	      (*info->print_address_func) (info->target, info);
 	      break;
-            }
+	    }
 	  else if (d[1] == '2')
 	    {
 	      info->target = (EXTRACT_CV_HWLP_UIMM5 (l)<<1) + pc; ++d;
