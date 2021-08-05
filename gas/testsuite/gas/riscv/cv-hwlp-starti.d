3f7a85e6f788b21e79d7488572cc81930e280e8e
Jessica Mills <jessica.mills@embecosm.com>
2021-08-05 08:49:39 +0000

    HWLP boundary bug, ALU encoding bug and general formatting fixes
    - Fix for bug relating to incorrect encoding of ALU sign and zero extend operations.
    - Fix for bug relating to incorrect HWLP branch offset upper boundaries.
    - Fix general white space and number formatting inconsistencies.
    
    gas/ChangeLog.COREV:
    
    	* config/tc-riscv.c (riscv_ip): Fix boundary bug for hardware loop
    	branch operands - upper boundary of long branch offset changed from
    	4094 to 8191 and short offset from 30 to 63.
    
    gas/testsuite/ChangeLog.COREV:
    
    	* gas/riscv/cv-alu-extbs.d: Fix test after encoding change.
    	* gas/riscv/cv-alu-extbs.s: Likewise.
    	* gas/riscv/cv-alu-extbz.d: Likewise.
    	* gas/riscv/cv-alu-exths.d: Likewise.
    	* gas/riscv/cv-alu-exthz.d: Likewise.
    	* gas/riscv/cv-alu-march-rv32i-xcorev.d: Amend test with branch
    	offset boundary change.
    	* gas/riscv/cv-hwlp-endi.d: Likewise.
    	* gas/riscv/cv-hwlp-endi.s: Likewise.
    	* gas/riscv/cv-hwlp-fail-operand-01.l: Likewise.
    	* gas/riscv/cv-hwlp-fail-operand-01.s: Likewise.
    	* gas/riscv/cv-hwlp-fail-operand-02.l: Likewise.
    	* gas/riscv/cv-hwlp-fail-operand-02.s: Likewise.
    	* gas/riscv/cv-hwlp-fail-operand-03.l: Likewise.
    	* gas/riscv/cv-hwlp-fail-operand-03.s: Likewise.
    	* gas/riscv/cv-hwlp-fail-operand-04.l: Likewise.
    	* gas/riscv/cv-hwlp-fail-operand-04.s: Likewise.
    	* gas/riscv/cv-hwlp-setup.d: Likewise.
    	* gas/riscv/cv-hwlp-setup.s: Likewise.
    	* gas/riscv/cv-hwlp-starti.d: Likewise.
    	* gas/riscv/cv-hwlp-starti.s: Likewise.
    
    include/ChangeLog.COREV:
    
    	* opcode/riscv-opc.h: Fix hexadecimal number format inconsistencies
    	for matches and masks.
    
    ld/testsuite/ChangeLog.COREV:
    
    	* ld-riscv-elf/ld-riscv-elf.exp: Renamed CORE-V hwlp tests to match
    	-march option for the instruction set.
    	* ld-riscv-elf/cv-hwloop-starti.s: Renamed to cv-hwlp-starti.s.
    	* ld-riscv-elf/cv-hwloop-starti.d: Renamed to cv-hwlp-starti.d.
    	* ld-riscv-elf/cv-hwloop-endi.d: Renamed to cv-hwlp-endi.d.
    	* ld-riscv-elf/cv-hwloop-endi.s: Renamed to cv-hwlp-endi.s.
    	* ld-riscv-elf/cv-hwloop-setup.d: Renamed to cv-hwlp-setup.d.
    	* ld-riscv-elf/cv-hwloop-setup.s: Renamed to cv-hwlp-setup.s.
    	* ld-riscv-elf/cv-hwloop-setupi.d: Renamed to cv-hwlp-setupi.d.
    	* ld-riscv-elf/cv-hwloop-setupi.s: Renamed to cv-hwlp-setupi.s.
    
    opcodes/ChangeLog.COREV:
    
    	* riscv-opc.c (MATCH_CV_EXTHS, MATCH_CV_EXTHZ, MATCH_CV_EXTBS)
    	(MATCH_CV_EXTBZ): Add new match.
    	(riscv_opcodes): Fix indentation inconsistencies.

--- gas/testsuite/gas/riscv/cv-hwlp-starti.d
+++ gas/testsuite/gas/riscv/cv-hwlp-starti.d
@@ -9,4 +9,4 @@ Disassembly of section .text:
 0+000 <target>:
 [ 	]+0:[ 	]+0000007b[ 	]+cv.starti[ 	]+0,0 <target>
 [ 	]+4:[ 	]+5e6000fb[ 	]+cv.starti[ 	]+1,bd0 +<target\+0xbd0>
-[ 	]+8:[ 	]+7ff0007b[ 	]+cv.starti[ 	]+0,1006 +<target\+0x1006>
+[ 	]+8:[ 	]+fff0007b[ 	]+cv.starti[ 	]+0,6 +<target\+0x6>
