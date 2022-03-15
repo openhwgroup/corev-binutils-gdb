19a04734ce7e986d10f0aef9b5b9ffbca933c3ce
Lin Sinan <sinan@isrc.iscas.ac.cn>
2022-03-15 15:14:50 +0800

    remove hardcoded address in testcases

--- ld/testsuite/ld-riscv-elf/cv-hwlp-endi.d
+++ ld/testsuite/ld-riscv-elf/cv-hwlp-endi.d
@@ -13,8 +13,8 @@ Disassembly of section \.text:
 .*:[[:space:]]+00008067[[:space:]]+ret
 
 .* <_start>:
-.*:[[:space:]]+0040107b[[:space:]]+cv.endi[[:space:]]+0,10060 <L2>
-.*:[[:space:]]+ff9ff0ef[[:space:]]+jal[[:space:]]+ra,10054 <func>
+.*:[[:space:]]+0040107b[[:space:]]+cv.endi[[:space:]]+0,.*[[:space:]]+<L2>
+.*:[[:space:]]+ff9ff0ef[[:space:]]+jal[[:space:]]+ra,.*[[:space:]]+<func>
 
 .* <L2>:
 .*:[[:space:]]+00000013[[:space:]]+nop
