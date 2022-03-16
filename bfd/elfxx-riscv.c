4d7e4fd8f592729a06d60e75e9665917d1f6cc6c
Lin Sinan <sinan@isrc.iscas.ac.cn>
2022-03-16 14:17:13 +0800

    Zcmp: add push, pop, popret and popretz

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -2514,6 +2514,8 @@ riscv_multi_subset_supports (riscv_parse_subset_t *rps,
 	    && riscv_subset_supports (rps, "m");
     case INSN_CLASS_ZCMB:
       return riscv_subset_supports (rps, "zcmb");
+    case INSN_CLASS_ZCMP:
+      return riscv_subset_supports (rps, "zcmp");
     default:
       rps->error_handler
         (_("internal: unreachable INSN_CLASS_*"));
@@ -2625,4 +2627,18 @@ riscv_multi_subset_supports_ext (riscv_parse_subset_t *rps,
         (_("internal: unreachable INSN_CLASS_*"));
       return NULL;
     }
+}
+
+/* get base sp adjustment */
+
+int
+riscv_get_base_spimm (insn_t opcode, riscv_parse_subset_t *rps)
+{
+  unsigned sp_alignment = 16;
+  unsigned reg_size = *(rps->xlen) / 8;
+  unsigned rlist = EXTRACT_BITS (opcode, OP_MASK_RLIST, OP_SH_RLIST);
+
+  unsigned min_sp_adj = (rlist - 3) * reg_size + (rlist == 15 ? reg_size : 0);
+  return ((min_sp_adj / sp_alignment) + (min_sp_adj % sp_alignment != 0))
+	        * sp_alignment;
 }
