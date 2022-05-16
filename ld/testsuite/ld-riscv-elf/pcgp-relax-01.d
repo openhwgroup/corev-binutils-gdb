88957f206e7e5d6708253f02fb4b026ad2d2d54a
jiawei <jiawei@iscas.ac.cn>
2022-05-16 15:49:55 +0800

    Update pcgp symbol names.

--- ld/testsuite/ld-riscv-elf/pcgp-relax-01.d
+++ ld/testsuite/ld-riscv-elf/pcgp-relax-01.d
@@ -12,5 +12,5 @@ Disassembly of section \.text:
 .*:[ 	]+[0-9a-f]+[ 	]+jal[ 	]+ra,[0-9a-f]+ <_start>
 .*:[ 	]+[0-9a-f]+[ 	]+addi[ 	]+a1,gp,\-[0-9]+ # [0-9a-f]+ <data_g>
 .*:[ 	]+[0-9a-f]+[ 	]+addi[ 	]+a2,gp,\-[0-9]+ # [0-9a-f]+ <data_g>
-.*:[ 	]+[0-9a-f]+[ 	]+addi[ 	]+a3,tp,0 # 0 <data_t>
+.*:[ 	]+[0-9a-f]+[ 	]+addi[ 	]+a3,tp,0 # 0 <__jvt_base\$>
 .*:[ 	]+[0-9a-f]+[ 	]+auipc[ 	]+a0,0x[0-9a-f]+
