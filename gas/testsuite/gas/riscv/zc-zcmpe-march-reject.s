d83ab45dd0714d1b79b5b3e1376e0b0b6f71f80d
Charlie Keaney <charlie.keaney@embecosm.com>
2022-07-29 15:39:23 +0100

    Added test case for failure/success on use of invalid/valid opcodes respectively for Zcmpe.
    	* gas/riscv/zc-zcmpe-march-accept.d: Added test case for success on use of valid opcodes for Zcmpe.
    	* gas/riscv/zc-zcmpe-march-accept.s: Added test case for success on use of valid opcodes for Zcmpe.
    	* gas/riscv/zc-zcmpe-march-reject.d: Added test case for failure on use of invalid opcodes for Zcmpe.
    	* gas/riscv/zc-zcmpe-march-reject.l: Added test case for failure on use of invalid opcodes for Zcmpe.
    	* gas/riscv/zc-zcmpe-march-reject.s: Added test case for failure on use of invalid opcodes for Zcmpe.

---
+++ gas/testsuite/gas/riscv/zc-zcmpe-march-reject.s
@@ -0,0 +1,28 @@
+# Test to ensure gas rejects those operations not within Zcb when running with Zcb march flags.
+zcb:
+    c.flw fa0, 0(a0)
+    c.flw fa0, 0(sp)
+    c.fsw fa0, 0(a0)
+    c.fsw fs2, 0(sp)
+    c.lbu x9,1(x8)
+    c.lh x9,2(x14)
+    c.lhu x9,2(x14)
+    c.sb x9,1(x8)
+    c.sh x9,2(x9)
+    c.zext.b x8
+    c.sext.b x15
+    c.zext.h x8
+    c.sext.h a0
+    c.zext.w x8
+    c.mul x15,x15
+    c.not x15
+    cm.lb x8,(x8)
+    cm.lbu x8,(x8)
+    cm.lh x8,2(x8)
+	cm.lhu x8,2(x8)
+    cm.sb x8,2(x10)
+    cm.sh x8,2(x8)
+    cm.mva01s s0, s7
+    cm.mvsa01 s0, s7
+    cm.jt 512
+    cm.jalt 8
