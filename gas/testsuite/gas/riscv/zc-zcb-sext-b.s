7dd39511a8604c48e08c0690ee7fc78fd646a604
Charlie Keaney <charlie.keaney@embecosm.com>
2022-07-29 15:19:49 +0100

    Added test case for failure or success on use of invalid or valid opcodes respectively for Zcb.

--- gas/testsuite/gas/riscv/zc-zcb-sext-b.s
+++ gas/testsuite/gas/riscv/zc-zcb-sext-b.s
@@ -1,12 +1,12 @@
 zcb:
-	# test zcb sign/zero extension insturction
+	# test zcb sign/zero extension instruction
 	c.zext.h x8
 	c.zext.h x15
 	c.zext.h x10
 	c.zext.h x9
 	c.zext.h x12
 
-	# test to compress sign/zero extension insturctions
+	# test to compress sign/zero extension instructions
 	zext.h x8,x8
 	zext.h x15,x15
 	zext.h x9,x9
---
+++ gas/testsuite/gas/riscv/zc-zcb-zbb-fail-march.d
@@ -0,0 +1,3 @@
+#as: -march=rv32g_zca_zcb_zbb
+#source: zc-zcb-zbb-fail-march.s
+#error_output: zc-zcb-zbb-fail-march.l
