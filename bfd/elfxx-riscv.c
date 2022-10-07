809c9cb101112086ff8acdb9b7f0174c833325ff
NandniJamnadas <nandni.jamnadas@embecosm.com>
2022-10-07 12:32:42 +0100

    RISC-V: Add support for XCVelw extension in CV32E40P
    Spec: https://docs.openhwgroup.org/projects/cv32e40p-user-manual/en/latest/instruction_set_extensions.html
    
    Contributors:
      Mary Bennett <mary.bennett@embecosm.com>
      Nandni Jamnadas <nandni.jamnadas@embecosm.com>
      Pietra Ferreira <pietra.ferreira@embecosm.com>
      Charlie Keaney
      Jessica Mills
      Craig Blackmore <craig.blackmore@embecosm.com>
      Simon Cook <simon.cook@embecosm.com>
      Jeremy Bennett <jeremy.bennett@embecosm.com>
      Helene Chelin <helene.chelin@embecosm.com>
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c (riscv_multi_subset_supports): Add `xcvelw`
              instruction class.
    	(riscv_multi_subset_supports_ext): Likewise.
    
    gas/ChangeLog:
    
            * doc/c-riscv.texi: Note XCVelw as an additional ISA extension
              for CORE-V.
            * testsuite/gas/riscv/cv-elw-fail.d: New test.
            * testsuite/gas/riscv/cv-elw-fail.l: New test.
            * testsuite/gas/riscv/cv-elw-fail.s: New test.
            * testsuite/gas/riscv/cv-elw-fail-march.d: New test.
    	* testsuite/gas/riscv/cv-elw-fail-march.l: New test.
            * testsuite/gas/riscv/cv-elw-fail-march.s: New test.
            * testsuite/gas/riscv/cv-elw-pass.d: New test.
            * testsuite/gas/riscv/cv-elw-pass.s: New test.
    
    opcodes/ChangeLog:
    
            * riscv-opc.c: (riscv_opcode) Add event load instructions.
    
    include/ChangeLog:
    
            * opcode/riscv-opc.h: Add corresponding MATCH and MASK
              instruction opcode macros.
            * opcode/riscv.h (riscv_insn_class): Add INSN_CLASS_XCVELW.

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1369,6 +1369,7 @@ static struct riscv_supported_ext riscv_supported_vendor_x_ext[] =
 {
   {"xcvmac",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xcvalu",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
+  {"xcvelw",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xtheadba",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xtheadbb",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
   {"xtheadbs",		ISA_SPEC_CLASS_DRAFT,	1, 0, 0 },
@@ -2576,6 +2577,8 @@ riscv_multi_subset_supports (riscv_parse_subset_t *rps,
       return riscv_subset_supports (rps, "xcvmac");
     case INSN_CLASS_XCVALU:
       return riscv_subset_supports (rps, "xcvalu");
+    case INSN_CLASS_XCVELW:
+      return riscv_subset_supports (rps, "xcvelw");
     case INSN_CLASS_XTHEADBA:
       return riscv_subset_supports (rps, "xtheadba");
     case INSN_CLASS_XTHEADBB:
@@ -2828,6 +2831,8 @@ riscv_multi_subset_supports_ext (riscv_parse_subset_t *rps,
       return "xcvmac";
     case INSN_CLASS_XCVALU:
       return "xcvalu";
+    case INSN_CLASS_XCVELW:
+      return "xcvelw";
     case INSN_CLASS_XTHEADBA:
       return "xtheadba";
     case INSN_CLASS_XTHEADBB:
