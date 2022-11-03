694c9202490172e6c5afad24f4a8a019e219a111
NandniJamnadas <nandni.jamnadas@embecosm.com>
2022-11-03 17:06:28 +0000

    Bit Manipulation MARCH Tests in GNU Assembler
    	* gas/testsuite/gas/riscv/cv-bitmanip-march-xcorev.d: COREV
    	  MARCH test.
    	* gas/testsuite/gas/riscv/cv-bitmanip-march-xcorev.s: Likewise.
    	* gas/testsuite/gas/riscv/cv-bitmanip-march-xcorevbitmanip.d:
    	  Bit Manipulation MARCH test.
    	* gas/testsuite/gas/riscv/cv-bitmanip-march-xcorevbitmanip.s:
              Likewise
    
    Signed-off-by: NandniJamnadas <nandni.jamnadas@embecosm.com>

---
+++ gas/testsuite/gas/riscv/cv-bitmanip-march-xcorev.s
@@ -0,0 +1,17 @@
+target: 
+    cv.extract x5, x10, 10, 20
+    cv.extractu x6, x9, 15, 20
+    cv.insert x7, x8, 20, 20
+    cv.bclr x8, x7, 25, 20
+    cv.bset x9, x6, 30, 20
+    cv.bitrev x10, x5, 5, 3
+    cv.extractr x5, x6, x7
+    cv.extractur x5, x6, x7
+    cv.insertr x5, x6, x7
+    cv.bclrr x5, x6, x7
+    cv.bsetr x5, x6, x7
+    cv.ror x5, x6, x7
+    cv.ff1 x6, x7
+    cv.fl1 x6, x7
+    cv.clb x6, x7
+    cv.cnt x6, x7
