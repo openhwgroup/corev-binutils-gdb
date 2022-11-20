7160cd88b0cf545e55170277f6905de90f7813b5
Lin Sinan <sinan.lin@linux.alibaba.com>
2022-11-20 23:10:36 +0800

    ZC: fix a unused case and test --zcmt-force-table-jump option.

---
+++ ld/testsuite/ld-riscv-elf/zc-zcmt-jump-table.d
@@ -0,0 +1,27 @@
+#name: test --zcmt-force-table-jump option
+#source: zc-zcmt-jump-table.s
+#as: -march=rv32im_zca_zcmt
+#ld: -nostdlib -static --zcmt-force-table-jump
+#objdump: -d
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+
+0+[0-9a-f]+ <bar>:
+.*:[ 	]+[0-9a-f]+[ 	]+ret
+
+0+[0-9a-f]+ <foo>:
+.*:[ 	]+[0-9a-f]+[ 	]+ret
+
+0+[0-9a-f]+ <_start>:
+.*:[ 	]+[0-9a-f]+[ 	]+cm.jalt[ 	]+64
+.*:[ 	]+[0-9a-f]+[ 	]+cm.jalt[ 	]+65
+.*:[ 	]+[0-9a-f]+[ 	]+ret
+	...
+
+0+[0-9a-f]+ <__jvt_base\$>:
+	...
+.*:[ 	]+[0-9a-f]+[ 	]+index 64[ 	]+[^<]+<foo>
+.*:[ 	]+[0-9a-f]+[ 	]+index 65[ 	]+[^<]+<bar>
