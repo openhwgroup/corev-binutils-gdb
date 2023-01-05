6e9248fd18c5d4626f62d9b3382dc39e7577664d
Nandni Jamnadas <nandni.jamnadas@embecosm.com>
2023-01-05 12:39:43 +0000

    Immediate Branching Version 2 Instructions and Tests Encodings Updated
    	include/opcode:
    	    * riscv-opc.h: Added corresponding MATCH and MASK
    	      macros for Immediate Branching.
    
    	gas/testsuite/gas/riscv:
    	    * gas/testsuite/gas/riscv/cv-bi-beqimm.d
    	    * gas/testsuite/gas/riscv/cv-bi-bneimm.d
    	    * gas/testsuite/gas/riscv/cv-bi-march-rv32i-xcorev.d
    
    Signed-off-by: Nandni Jamnadas <nandni.jamnadas@embecosm.com>

--- gas/testsuite/gas/riscv/cv-bi-beqimm.d
+++ gas/testsuite/gas/riscv/cv-bi-beqimm.d
@@ -7,6 +7,6 @@
 Disassembly of section .text:
 
 0+000 <foo>:
-[ 	]+0:[ 	]+0102a063[ 	]+cv.beqimm[ 	]+t0,-16,0 +<foo>
-[ 	]+4:[ 	]+fe5eaee3[ 	]+cv.beqimm[ 	]+t4,5,0 +<foo>
-[ 	]+8:[ 	]+fef3ace3[ 	]+cv.beqimm[ 	]+t2,15,0 +<foo>
+[ 	]+0:[ 	]+0102e00b[ 	]+cv.beqimm[ 	]+t0,-16,0 +<foo>
+[ 	]+4:[ 	]+fe5eee8b[ 	]+cv.beqimm[ 	]+t4,5,0 +<foo>
+[ 	]+8:[ 	]+fef3ec8b[ 	]+cv.beqimm[ 	]+t2,15,0 +<foo>
--- gas/testsuite/gas/riscv/cv-bi-march-rv32i-xcorev.d
+++ gas/testsuite/gas/riscv/cv-bi-march-rv32i-xcorev.d
@@ -7,5 +7,5 @@
 Disassembly of section .text:
 
 0+000 <foo>:
-[ 	]+0:[ 	]+0013a063[ 	]+cv.beqimm[ 	]+t2,1,0 +<foo>
-[ 	]+4:[ 	]+ff02bee3[ 	]+cv.bneimm[ 	]+t0,-16,0 +<foo>
+[ 	]+0:[ 	]+0013e00b[ 	]+cv.beqimm[ 	]+t2,1,0 +<foo>
+[ 	]+4:[ 	]+ff02fe8b[ 	]+cv.bneimm[ 	]+t0,-16,0 +<foo>
