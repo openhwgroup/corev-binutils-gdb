1a14ec6646c4994edec1ff24376858a3356d1cb1
Mary Bennett <mary.bennett@embecosm.com>
2023-01-31 13:23:32 +0000

    Updated hardware loop instruction encodings
    These hardware loop instructions have different encodings for
    CV32E40Pv2 compared to CV32E40Pv1. These new encodings can be
    found at:
    https://github.com/openhwgroup/cv32e40p/blob/master/docs/source/instruction_set_extensions.rst
    
    CV32E40Pv2 changed how uimml in cv.starti, cv.endi, and cv.setup
    was encoded. The same is true for uimmS in cv.setupi. This
    changed the relocations for each immediate as well.
    
    Both the GAS and linker tests were updated too.
    
    Files Changed:
    
    bfd:
     * elfxx-riscv.c: Changed the CORE-V hardware loop relocations to
       rightshift 2.
    
    gas/config:
     * tc-riscv.c: Changed encoding of uimmL and uimmS in hardware loop
       instructions.
    
    gas/testsuite/gas/riscv:
     * cv-hwlp-endi.d: Updated tests.
     * cv-hwlp-endi.s: Likewise.
     * cv-hwlp-fail-operand-01.l: Likewise.
     * cv-hwlp-fail-operand-01.s: Likewise.
     * cv-hwlp-fail-operand-02.l: Likewise.
     * cv-hwlp-fail-operand-03.l: Likewise.
     * cv-hwlp-fail-operand-03.s: Likewise.
     * cv-hwlp-fail-operand-04.l: Likewise.
     * cv-hwlp-fail-operand-10.s: Likewise.
     * cv-hwlp-march-rv32i-xcorev.d: Likewise.
     * cv-hwlp-setup.d: Likewise.
     * cv-hwlp-setup.s: Likewise.
     * cv-hwlp-setupi.d: Likewise.
     * cv-hwlp-setupi.s: Likewise.
     * cv-hwlp-starti.d: Likewise.
     * cv-hwlp-starti.s: Likewise.
    
    ld/testsuite/ld-riscv-elf:
     * cv-hwlp-endi.d: Updated tests.
     * cv-hwlp-setup.d: Likewise.
     * cv-hwlp-setupi.d: Likewise.
     * cv-hwlp-starti.d: Likewise.
    
    opcodes:
     * riscv-dis.c: Changed how the relocation is printed.

--- gas/testsuite/gas/riscv/cv-hwlp-setup.d
+++ gas/testsuite/gas/riscv/cv-hwlp-setup.d
@@ -8,5 +8,6 @@ Disassembly of section .text:
 
 0+000 <target>:
 [ 	]+0:[ 	]+0002c72b[ 	]+cv.setup[ 	]+0,t0,0 <target>
-[ 	]+4:[ 	]+0f4f47ab[ 	]+cv.setup[ 	]+1,t5,1ec <target\+0x1ec>
-[ 	]+8:[ 	]+fff5472b[ 	]+cv.setup[ 	]+0,a0,6 <target\+0x6>
+[ 	]+4:[ 	]+07af47ab[ 	]+cv.setup[ 	]+1,t5,1ec <target\+0x1ec>
+[ 	]+8:[ 	]+7ff5472b[ 	]+cv.setup[ 	]+0,a0,2004 <target\+0x2004>
+[ 	]+c:[ 	]+fffe47ab[ 	]+cv.setup[ 	]+1,t3,8 <target\+0x8>
