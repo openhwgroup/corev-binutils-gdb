831fd151bd4143f136562182e13bfded45cd0d48
Simon Cook <simon.cook@embecosm.com>
2023-05-02 12:45:45 +0100

    RISC-V: Fix .option arch compatibility with zc*
    This fixes an issue whereby .option arch with the zc extensions can
    result in gas refusing to accept otherwise valid instructions because
    the base C extension is not enabled.

--- gas/config/tc-riscv.c
+++ gas/config/tc-riscv.c
@@ -4948,6 +4948,8 @@ s_riscv_option (int x ATTRIBUTE_UNUSED)
       riscv_update_subset (&riscv_rps_as, "-c");
       riscv_reset_subsets_list_arch_str ();
       riscv_set_rvc (false);
+      if (riscv_subset_supports (&riscv_rps_as, "zca"))
+	riscv_set_rvc (true);
     }
   else if (strcmp (name, "pic") == 0)
     riscv_opts.pic = true;
@@ -4970,7 +4972,8 @@ s_riscv_option (int x ATTRIBUTE_UNUSED)
       riscv_reset_subsets_list_arch_str ();
 
       riscv_set_rvc (false);
-      if (riscv_subset_supports (&riscv_rps_as, "c"))
+      if (riscv_subset_supports (&riscv_rps_as, "c")
+	  || riscv_subset_supports (&riscv_rps_as, "zca"))
 	riscv_set_rvc (true);
 
       if (riscv_subset_supports (&riscv_rps_as, "ztso"))
