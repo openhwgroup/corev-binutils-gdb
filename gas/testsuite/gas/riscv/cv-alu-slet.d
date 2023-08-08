361ec0dfad22cb4dfc470ecdf0f3bf6012ecee1f
Mary Bennett <mary.bennett@embecosm.com>
2023-08-08 15:07:08 +0100

    Renaming of cv.slet to cv.sle
    For issue [#100](https://github.com/openhwgroup/corev-binutils-gdb/issues/100)
    
    Files Changes:
    
      * gas/testsuite/gas/riscv/cv-alu-slet.d: Changed the name of cv.slet.
      * gas/testsuite/gas/riscv/cv-alu-slet.s: Likewise.
      * gas/testsuite/gas/riscv/cv-alu-sletu.d: Likewise.
      * gas/testsuite/gas/riscv/cv-alu-sletu.s: Likewise.
      * opcodes/riscv-opc.c: Likewise.
    
    Signed-off-by: Mary Bennett <mary.bennett@embecosm.com>

--- gas/testsuite/gas/riscv/cv-alu-slet.d
+++ gas/testsuite/gas/riscv/cv-alu-slet.d
@@ -7,6 +7,6 @@
 Disassembly of section .text:
 
 0+000 <target>:
-[ 	]+0:[ 	]+527332ab[ 	]+cv.slet[ 	]+t0,t1,t2
-[ 	]+4:[ 	]+53f3beab[ 	]+cv.slet[ 	]+t4,t2,t6
-[ 	]+8:[ 	]+526f3e2b[ 	]+cv.slet[ 	]+t3,t5,t1
+[ 	]+0:[ 	]+527332ab[ 	]+cv.sle[ 	]+t0,t1,t2
+[ 	]+4:[ 	]+53f3beab[ 	]+cv.sle[ 	]+t4,t2,t6
+[ 	]+8:[ 	]+526f3e2b[ 	]+cv.sle[ 	]+t3,t5,t1
--- gas/testsuite/gas/riscv/cv-alu-sletu.d
+++ gas/testsuite/gas/riscv/cv-alu-sletu.d
@@ -7,6 +7,6 @@
 Disassembly of section .text:
 
 0+000 <target>:
-[ 	]+0:[ 	]+547332ab[ 	]+cv.sletu[ 	]+t0,t1,t2
-[ 	]+4:[ 	]+55f3beab[ 	]+cv.sletu[ 	]+t4,t2,t6
-[ 	]+8:[ 	]+546f3e2b[ 	]+cv.sletu[ 	]+t3,t5,t1
+[ 	]+0:[ 	]+547332ab[ 	]+cv.sleu[ 	]+t0,t1,t2
+[ 	]+4:[ 	]+55f3beab[ 	]+cv.sleu[ 	]+t4,t2,t6
+[ 	]+8:[ 	]+546f3e2b[ 	]+cv.sleu[ 	]+t3,t5,t1
